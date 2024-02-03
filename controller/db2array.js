bd3c6e7be2c94842380b58f8d0df3ab9a38840e2
AngeloNorelli <angnorelli@gmail.com>
2024-02-03 17:37:15 +0100

    db2array działające

--- controller/db2array.js
+++ controller/db2array.js
@@ -1,37 +1,35 @@
 const { queryAsync } = require('./database');
 const fs = require ('fs');
 
-const PrefabDatabase = (function() {
-    return {
-        DB2Array
-    };
-    async function DB2Array (query, values, fileName) {
-        try {
-            const result = await queryAsync(query, values);
+async function DB2Array (query, values, fileName) {
+    try {
+        const result = await queryAsync(query, values);
 
-            console.log(result);
-            if (result.length == 0) {
-                return { status: 403, message: 'No values in that table.' };
-            }
-
-            const gamesData = []; 
-            if (result) {
-                for (let i = 0; i < result.length; i++) {
-                    let row = result[i];
-                    gamesData.push(row);
-                }
-            }
-            
-            console.log(gamesData);
-            let jsonData = JSON.stringify(gamesData, null, 2);
-            fs.writeFileSync('' + fileName, `const gamesData = ${jsonData};`);
-
-            return { status: 200, message: 'Data aquired' };
-        } catch (error) {
-            console.log(error);
-            return { status: 500, message: 'Internal Server Error' };
+        console.log(result);
+        if (result.length == 0) {
+            return { status: 403, message: 'No values in that table.' };
         }
-    }
-})();
 
-PrefabDatabase.DB2Array("SELECT * FROM games WHERE games.verified = '1'", "page_yourGames.js");
+        const gamesData = []; 
+        if (result) {
+            for (let i = 0; i < result.length; i++) {
+                let row = result[i];
+                gamesData.push(row);
+            }
+        }
+        
+        console.log(gamesData);
+        let jsonData = JSON.stringify(gamesData, null, 2);
+        console.log(jsonData)
+        fs.writeFileSync(`/public/code/` + fileName, `const gamesData = ${jsonData};`);
+
+        return { status: 200, message: 'Data aquired' };
+    } catch (error) {
+        console.log(error);
+        return { status: 500, message: 'Internal Server Error' };
+    }
+}
+
+module.exports = {
+    DB2Array
+};
