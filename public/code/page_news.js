c5c042519b6c2c10b9947f6f7c98b8976bafd1f6
AngeloNorelli <angnorelli@gmail.com>
2024-02-05 21:09:57 +0100

    dówno

--- public/code/page_news.js
+++ public/code/page_news.js
@@ -2,11 +2,19 @@ const gamesData = [
   {
     "gameID": 1,
     "name": "Game 1",
-    "verified": 1
+    "description": "description of the game 1",
+    "isDownloaded": 1
   },
   {
     "gameID": 2,
     "name": "Game 2",
-    "verified": 1
+    "description": "description of the game 2",
+    "isDownloaded": 1
+  },
+  {
+    "gameID": 4,
+    "name": "twoja stara siada na butli od vanisha",
+    "description": null,
+    "isDownloaded": null
   }
 ];
--- public/code/page_yourGames.js
+++ public/code/page_yourGames.js
@@ -1,34 +1,29 @@
 const gamesData = [
   {
     "ID": 1,
-    "userID": 3,
+    "userID": 10,
     "gameID": 1,
     "isDownloaded": 1,
     "name": "Game 1",
-    "verified": 1
-  },
-  {
-    "ID": 3,
-    "userID": 14,
-    "gameID": 1,
-    "isDownloaded": 1,
-    "name": "Game 1",
-    "verified": 1
+    "verified": 1,
+    "description": "description of the game 1"
   },
   {
     "ID": 2,
-    "userID": 3,
+    "userID": 10,
     "gameID": 2,
     "isDownloaded": 1,
     "name": "Game 2",
-    "verified": 1
+    "verified": 1,
+    "description": "description of the game 2"
   },
   {
     "ID": 4,
-    "userID": 4,
+    "userID": 10,
     "gameID": 3,
     "isDownloaded": 0,
     "name": "Game 3",
-    "verified": 0
+    "verified": 0,
+    "description": "description of the game 3"
   }
 ];
